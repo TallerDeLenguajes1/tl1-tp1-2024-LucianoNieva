1a4f134db1a8294125f05dbb97f6e5032fe60367
Luciano Fabrizio Nieva <Lucianonieva49@gmail.com>
2024-04-02 23:37:22 -0300

    ejercicio 2.4 completado y corregido

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -1,74 +1,74 @@
 #include <stdio.h>
 
-int CuadradoNumero(int n)
+int CuadradoNumero(int * n)
 {
     int elevadoCuadrado;
 
-    elevadoCuadrado = n * n;
+    elevadoCuadrado = (*n) * (*n);
 
     return elevadoCuadrado;
 }
 
-void CuadradoNumeroVoid(int n)
+void CuadradoNumeroVoid(int * n)
 {
     int elevadoCuadrado;
 
-    elevadoCuadrado = n * n;
+    elevadoCuadrado = (*n) * (*n);
 
     printf("El numero elevado al cuadrado es:%d\n", elevadoCuadrado);
 }
 
-void mostrar(int n)
+void mostrar(int * n)
 {
 
     printf("La direccion de memoria es:%p\n", &n);
 
-    printf("El contenido de la variable es:%d\n", n);
+    printf("El contenido de la variable es:%d\n", *n);
 }
 
-void invertir(int a, int b)
+void invertir(int * a, int * b)
 {
     int aux;
 
-    aux = a;
+    aux = *a;
 
-    a = b;
+    *a = *b;
 
-    b = aux;
+    *b = aux;
 
-    printf("El valor de a es:%d\n", a);
-    printf("El valor de b es:%d\n", b);
+    printf("El valor de a es:%d\n", *a);
+    printf("El valor de b es:%d\n", *b);
 }
 
-void orden(int a, int b)
+void orden(int * a, int * b)
 {
 
     int aux;
 
-    if (a > b)
+    if (*a > *b)
     {
-        aux = a;
+        aux = *a;
 
-        a = b;
+        *a = *b;
 
-        b = aux;
+        *b = aux;
     }
     else
     {
 
-        a = a;
+        *a = *a;
 
-        b = b;
+        *b = *b;
     }
 
-    printf("El valor mas chico es:%d\n", a);
-    printf("El valor mas grande es:%d\n", b);
+    printf("El valor mas chico es:%d\n", *a);
+    printf("El valor mas grande es:%d\n", *b);
 }
 
 int main()
 {
 
-     int a, b;
+     int a, b, n;
 
     // Solicitar al usuario que ingrese los valores
     printf("Ingrese el valor de a: ");
@@ -76,19 +76,22 @@ int main()
     printf("Ingrese el valor de b: ");
     scanf("%d", &b);
 
-    invertir(a, b);
+    invertir(&a, &b);
 
-    orden(a,b);
+    orden(&a,&b);
+
+    printf("Ingrese el numero n: ");
+    scanf("%d",&n);
 
     int elevadoCuadrado;
 
-    elevadoCuadrado = CuadradoNumero(6);
+    elevadoCuadrado = CuadradoNumero(&n);
 
     printf("El cuadrado es:%d\n", elevadoCuadrado);
 
-    CuadradoNumeroVoid(10);
+    CuadradoNumeroVoid(&n);
 
-    mostrar(9);
+    mostrar(&n);
 
     
 
